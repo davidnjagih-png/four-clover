4c0e6b610b62b9f251f61fc95e6523e76ed0f7bd
davidnjagih-png <david.njagih@student.moringaschool.com>
2026-09-22 11:39:49 +0300

    SEO initialization

--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
-import "./style.css";
-
-// Mobile menu toggle
 const menuToggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
 menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("hidden"); // hide/show on mobile
-  menu.classList.toggle("flex"); // ensure flex layout when visible
-  menuToggle.classList.toggle("active"); // rotate icon when active
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("flex-col");
+
+  menuToggle.classList.toggle("active");
+});
+
+// Close menu when link clicked
+document.querySelectorAll("#menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    if (window.innerWidth < 768) {
+      menu.classList.add("hidden");
+      menu.classList.remove("flex");
+      menu.classList.remove("flex-col");
+      menuToggle.classList.remove("active");
+    }
+  });
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href"))?.scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
